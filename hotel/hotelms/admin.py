21bb82c733ed2a030fd627b9e6914fea9efb5ebd
priti pandey <pandeypriti015>
2019-10-18 22:21:21 +0530

    hotel validation is done

--- hotel/hotelms/admin.py
+++ hotel/hotelms/admin.py
@@ -24,3 +24,60 @@ admin.site.register(Manager,ManagerAdmin)
 admin.site.register(Room,RoomAdmin)
 admin.site.register(Guest,GuestAdmin)
 admin.site.register(Record,RecordAdmin)
+
+
+
+from django.contrib import admin
+from django import forms
+
+from .models import Hotel,Manager,Room,Guest,Booking
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('Name','City',)
+
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ('Name','Number',)
+
+# class TypeAdmin(admin.ModelAdmin):
+#     list_display = ('Type_1','Type_2','Type_3',)
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('Number','Type','Bed','Price','Availability',)
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('Name','Number','Address',)
+
+
+# class BookingAdminForm(forms.ModelForm):
+#
+#     def __init__(self, *args, **kwargs):
+#         super(BookingAdminForm, self).__init__(*args, **kwargs)
+#
+#     def clean(self):
+#         room = self.cleaned_data.get('room')
+#         is_available = self.cleaned_data.get('is_available')
+#         if not is_available:
+#             if room.Available == 0:
+#                 raise forms.ValidationError("Room is not available",code="invalid")
+#             return self.cleaned.data
+#
+#     def save(self, commit=True):
+#         return super(BookingAdminForm, self).save(commit)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('Guest_Name','Guests','date_of_book','date_of_cancel',
+                    'Check_IN','Check_OUT','bill','checked_out','Room_Number',)
+    #form = BookingAdminForm
+
+# class BillAdmin(admin.ModelAdmin):
+#     list_display = ('Check_IN','Guest_Name','Type','Check_OUT','Invoice',)
+
+
+# Register your models here.
+admin.site.register(Hotel,HotelAdmin)
+admin.site.register(Manager,ManagerAdmin)
+#admin.site.register(Room_Type,TypeAdmin)
+admin.site.register(Room,RoomAdmin)
+admin.site.register(Guest,GuestAdmin)
+admin.site.register(Booking,BookingAdmin)
+#admin.site.register(Bill,BillAdmin)
--- hotel/hotelms/models.py
+++ hotel/hotelms/models.py
@@ -78,3 +78,115 @@ class Record(models.Model):
 
 
 
+from django.db import models
+from datetime import date, timedelta, datetime
+from django.db.models import signals
+from django.db.models.signals import pre_save,post_save
+from django.dispatch import receiver
+
+#Create your models here.
+
+class Hotel(models.Model):
+    Name = models.CharField(max_length=20)
+    City = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.Name)
+
+class Manager(models.Model):
+    Name = models.CharField(max_length=20)
+    Number = models.IntegerField()
+
+    def __str__(self):
+        return str(self.Name)
+
+# class Room_Type(models.Model):
+#     Type_1 = models.CharField(max_length=20)
+#     Type_2 = models.CharField(max_length=20)
+#     Type_3 = models.CharField(max_length=20)
+
+    # def __str__(self):
+    #     return str(self.Type_1) + str(self.Type_2) + str(self.Type_3)
+
+class Room(models.Model):
+    Number = models.IntegerField()
+    Type = models.CharField(max_length=20)
+    Bed = models.IntegerField()
+    Price = models.IntegerField()
+    Availability = models.BooleanField()
+
+
+    def __str__(self):
+        return str(self.Type)
+
+    def checked_out(self):
+        if self.Number > 0:
+            return True
+        else:
+            return False
+
+    checked_out.boolean = True
+    checked_out.short_description = 'checked_out'
+
+
+class Guest(models.Model):
+    Name = models.CharField(max_length=20)
+    Number = models.IntegerField()
+    Address = models.TextField()
+
+    def __str__(self):
+        return str(self.Name)
+
+class Booking(models.Model):
+    Guest_Name = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    Room_Number = models.ForeignKey(Room, on_delete=models.CASCADE)
+    # Type = models.ForeignKey(Room,on_delete=models.CASCADE)
+    Guests = models.IntegerField()
+    date_of_book = models.DateField()
+    date_of_cancel = models.DateField(null=True,blank=True)
+    Check_IN = models.DateField()
+    Check_OUT = models.DateField()
+    # Invoice = models.IntegerField(default=0)
+    #Available = models.IntegerField()
+    checked_out = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.Guest_Name)
+
+    def bill(self):
+        if self.checked_out:
+            timedelta1 = self.Check_OUT - self.Check_IN
+            days = timedelta1.days
+            return days * self.Room_Number.Price
+
+
+        # if self.Check_IN is not None:
+        #     self.Invoice = self.Check_OUT - self.Check_IN
+        #     if self.Invoice.days > 0:
+        #         return self.Invoice.days * self.Type.Price
+        #     else:
+        #         return 0
+
+@receiver(signals.post_save,sender=Booking)
+def update(sender,instance,created,**kwargs):
+    if not instance.Check_OUT:
+        if created:
+            avail = instance.Check_IN.Availability
+            if avail > 0:
+                avail -= 1
+                room = instance.Check_IN
+                room.Availability = avail
+                if avail <= 0:
+                    room.availability = False
+                room.save()
+
+        else:
+            avail = instance.Check_IN.Availability
+            room = instance.Check_IN
+            room.availability = avail + 1
+            room.save()
+
+
+
+
+
